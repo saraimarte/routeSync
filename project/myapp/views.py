e4aa1583cff7fb65bb9a82ef5d18a49349e539ad
Sarai Marte <smartepujols@gmail.com>
2023-09-28 21:02:22 -0400

    Made Form. Struggling w View Function

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "base.html")
+    if request.method == "POST" and request.FILES['file'] and request.FILES['file2'] and 'key' in request.POST:
+        python_file_path = "./route.py"
+        exec(open(python_file_path).read())
+        return render(request, "home.html")
+    else:
+        return render(request, "home.html")
